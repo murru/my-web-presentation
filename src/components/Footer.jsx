385c6c6bd061026cc2fa181028038bac4311c229
Ernesto Murrugarra <ernestomurrudev@gmail.com>
2022-10-03 15:59:11 -0400

    Changed footer, added René Centeno design editor and made skills section responsive.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,14 @@ import React from "react"
 export default function Footer() {
   return (
     <footer className="content__footer">
-      <p className="w3-large">Developed by Eng. Ernesto Murrugarra</p>
+      <div className="authors">
+        <div>
+          <p className="w3-large">Developed by Eng. Ernesto Murrugarra</p>
+        </div>
+        <div>
+          <p className="w3-large">Skills logotypes edited by <a href="https://www.instagram.com/renecent3no/" target="_blank">René Centeno</a></p>
+        </div>
+      </div>
       <p className="w3-large">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" className="w3-hover-text-green" rel="noreferrer">w3.css</a> & <a href="https://fontawesome.com/" target="_blank" className="w3-hover-text-green" rel="noreferrer">Fontawesome</a></p>
     </footer>
   )
